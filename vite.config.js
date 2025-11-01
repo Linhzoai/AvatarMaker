5e0d068051835c1982dd315c475e7717dc35144a
Linhzoai <Linhbarao@gmail.com>
2025-11-01 22:01:55 +0700

    commit lan 5

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// ⚙️ Cấu hình cho GitHub Pages
 export default defineConfig({
   plugins: [react()],
+  base: '/AvatarMaker/', // 👈 thay AvatarMaker bằng đúng tên repo của bạn
 })
